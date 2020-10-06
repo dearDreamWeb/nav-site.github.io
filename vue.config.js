e2a2c5c891e0913ee91e7dfef1727fbbc9e48ca6
wxb <m18837120047@163.com>
2020-10-06 12:25:40 +0800

    v0.1.7 完成

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,14 @@ module.exports = {
             'AMap': 'AMap' // 高德地图配置
         }
     },
+    //修改或新增html-webpack-plugin的值，在index.html里面能读取htmlWebpackPlugin.options.title
+    chainWebpack: config => {
+        config.plugin('html')
+            .tap(args => {
+                args[0].title = "奇迹导航";
+                return args;
+            })
+    },
     devServer: {
         proxy: {
             '/articles': {
